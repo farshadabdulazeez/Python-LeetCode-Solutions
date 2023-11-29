1c02d3e5eefa7e19ea032e0fb892ca694cf386e1
unknown <farshadabdulazeez@gmail.com>
2023-11-29 19:26:48 +0530

    Another Solution for Question 1

--- python_solutions_with_explanation/Question_1.py
+++ python_solutions_with_explanation/Question_1.py
@@ -2,9 +2,7 @@
 
 '''
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
 '''
@@ -21,29 +19,30 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        nums_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in nums_dict:
+                return [nums_dict[complement], i]
+            nums_dict[num] = i
 
 
 '''
-
 
 
 # Approach
 
 '''
-
-The approach used here is a nested loop. The outer loop iterates through each element in the nums list, and the inner loop iterates through the elements that come after the current element in the outer loop. For each pair of indices (i, j), the function checks if the corresponding elements in the array add up to the target.
-
-If a pair is found, the function immediately returns the indices as a list [i, j]. This approach assumes that there is exactly one solution, and it doesn't handle cases where there might be multiple valid pairs..
+The nums_dict dictionary is used to store the elements of nums and their corresponding indices.
+The for loop iterates through each element in the nums list, and for each element, it calculates the complement (the value needed to reach the target).
+If the complement is already in nums_dict, it means a pair of indices has been found, and the function returns those indices.
+If not, the current element and its index are added to nums_dict.
+The function returns the result as soon as a pair is found, making it an early exit algorithm.
 
 Time complexity:
- O(n^2)
+O(n)
 
 Space complexity:
-O(1)
+O(n)
 
 '''
-
